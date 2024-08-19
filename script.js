06a5b0cf3ef2f1745d63d3f450d2417cc9223520
neerajdhurandher <neerajdhurandar@gmail.com>
2024-08-19 16:39:12 +0530

    add zero day case animation, bye type writer effect & code restructure

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const targetDate = "August 20, 2024";
+
+let page_heading_text = "Bye Bye Saumya!"
+let page_heading_element = document.getElementById("page-heading")
+let cloud_container_element = document.querySelector(".cloud-container")
+let main_container_element = document.querySelector(".main-container")
+let page = document.querySelector(".date-page");
+let count = 0;
+const maxCount = 5;
+let daysLeft = undefined
+
 function calculateDaysLeft(targetDate) {
     const today = new Date();
     const futureDate = new Date(targetDate);
@@ -5,9 +16,11 @@ function calculateDaysLeft(targetDate) {
     const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
     return daysLeft;
 }
+
 function set_remaining_date(){
-    const targetDate = "August 20, 2024";
-    let daysLeft = calculateDaysLeft(targetDate);
+    if (daysLeft == undefined){
+        daysLeft = calculateDaysLeft(targetDate);
+    }
     if(daysLeft < 0){
         daysLeft = 0
     }
@@ -22,14 +35,6 @@ function updateDateTime() {
     footer.textContent = `Today's Date and Time: ${formattedDateTime}`;
 }
 
-updateDateTime();
-setInterval(updateDateTime, 1000); // Update every second
-
-
-
-let page = document.querySelector(".date-page");
-
-
 function hidden(){
     page.style.transform = 'translate(-30px,150px)';
     page.style.opacity = "0";
@@ -43,8 +48,7 @@ function visible(){
     }, 400)
 }
 
-let count = 0;
-const maxCount = 5;
+
 
 const intervalId = setInterval(() => {
   if (count < maxCount) {
@@ -55,3 +59,61 @@ const intervalId = setInterval(() => {
     clearInterval(intervalId);
   }
 }, 700);
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function add_character(alphabet, element) {
+    const node = document.createTextNode(alphabet);
+    const para = document.createElement("span");
+    para.appendChild(node);
+    element.appendChild(para);
+    return para;
+}
+
+async function type_writer(word, element, repeat) {
+    let node_list = []
+    for (let char of word) {
+        await sleep(200);
+        try {
+            node = add_character(char, element)
+            node_list.push(node)
+        } catch { }
+    }
+
+    if (repeat) {
+        await sleep(1500)
+        for (let i = node_list.length - 1; i > -1; i--) {
+            try {
+                element.removeChild(node_list[i])
+                await sleep(200);
+            } catch { }
+        }
+        for (let i = 0; i < node_list.length; i++) {
+            try {
+                element.appendChild(node_list[i])
+                await sleep(200);
+            } catch { }
+        }
+    }
+
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000); // Update every second
+type_writer(page_heading_text, page_heading_element, false)
+
+daysLeft = calculateDaysLeft(targetDate);
+
+if (daysLeft < 0){
+    main_container_element.style.display = "none" 
+    cloud_container_element.style.display = "block" 
+}else{
+    main_container_element.style.display = "block" 
+    cloud_container_element.style.display = "none" 
+
+}
+
+
